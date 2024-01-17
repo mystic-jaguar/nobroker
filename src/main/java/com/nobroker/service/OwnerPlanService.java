3b2d0e2a062b932c2579fc628e4b52f0902da5c9
Jay <jaywarale1@gmail.com>
2024-01-17 16:45:01 +0530

    Nobroker application

---
+++ src/main/java/com/nobroker/service/OwnerPlanService.java
@@ -0,0 +1,11 @@
+package com.nobroker.service;
+
+import com.nobroker.payload.OwnerPlanDto;
+
+import java.util.List;
+
+public interface OwnerPlanService {
+    public OwnerPlanDto createOwnerPlans(OwnerPlanDto ownerPlanDto);
+
+    List<OwnerPlanDto> getAllOwnerPlans();
+}
